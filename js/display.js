fc4594aa7d75ddf0ec471eebffbcfbe4805d0eb0
keneankit01 <47526753+keneankit01@users.noreply.github.com>
2021-02-08 14:07:51 +0530

    first commit

---
+++ js/display.js
@@ -0,0 +1,89 @@
+var a;
+function show_hide0()
+{
+if(a==1)
+{
+document.getElementById("introduction").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("introduction").style.display="none";
+return a=1;
+}
+}
+
+var a;
+function show_hide1()
+{
+if(a==1)
+{
+document.getElementById("Downloads").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("Downloads").style.display="none";
+return a=1;
+}
+}
+
+var a;
+function show_hide2()
+{
+if(a==1)
+{
+document.getElementById("Desginers").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("Desginers").style.display="none";
+return a=1;
+}
+}
+
+var a;
+function show_hide3()
+{
+if(a==1)
+{
+document.getElementById("Supporteddevices").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("Supporteddevices").style.display="none";
+return a=1;
+}
+}
+
+var a;
+function show_hide4()
+{
+if(a==1)
+{
+document.getElementById("SCREENSHOTS").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("SCREENSHOTS").style.display="none";
+return a=1;
+}
+}
+
+var a;
+function show_hide5()
+{
+if(a==1)
+{
+document.getElementById("OURTEAM").style.display="inline";
+return a=0;
+}
+else 
+{
+document.getElementById("OURTEAM").style.display="none";
+return a=1;
+}
+}
